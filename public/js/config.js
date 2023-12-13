a662a3a4afaa75f0b722537409c997361e1ca349
jm <jmarmucki@tm1.edu.pl>
2023-12-13 10:40:19 +0100

    Update: change config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_Qh48UIZ1J', // e.g. eu-east-2_uXboG5pAb
-        userPoolClientId: '15hhvh7cnct9imuatln22p82hr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
+        userPoolId: 'eu-west-1_avVZXqTR1',
+        userPoolClientId: '46f3v0ilvn2vdip27ikg7mpff6',
+        region: 'eu-west-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://l1sqpm0kh3.execute-api.eu-west-1.amazonaws.com/prod'
     }
 };
